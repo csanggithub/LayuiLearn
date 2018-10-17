db571a011f3734dfe85ce9947bd626ca5855e716
csang <csang@EL163>
2018-10-17 10:20:47 +0800

    update

--- Web/Controllers/AccountController.cs
+++ Web/Controllers/AccountController.cs
@@ -8,6 +8,11 @@ using System.Web.Mvc;
 using System.Web.Security;
 using Web;
 using Entitys.ViewModels;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using Entitys.Models;
+using System.IO;
+using System.Text;
 
 namespace Web.Controllers
 {
@@ -16,12 +21,14 @@ namespace Web.Controllers
         private readonly IUserServices _iUserServices;
         private readonly IFunctionServices _iFunctionServices;
         private readonly IUserFunctionServices _iUserFunctionServices;
+        private readonly IDistrictInfoServices _iDistrictInfoServices;
 
-        public AccountController(IUserServices iUserServices, IFunctionServices iFunctionServices, IUserFunctionServices iUserFunctionServices)
+        public AccountController(IUserServices iUserServices, IFunctionServices iFunctionServices, IUserFunctionServices iUserFunctionServices, IDistrictInfoServices iDistrictInfoServices)
         {
             _iUserServices = iUserServices;
             _iFunctionServices = iFunctionServices;
             _iUserFunctionServices = iUserFunctionServices;
+            _iDistrictInfoServices = iDistrictInfoServices;
         }
 
         // GET: Account
@@ -30,9 +37,60 @@ namespace Web.Controllers
             return View();
         }
 
+        private void AddDis()
+        {
+            byte[] byData = new byte[130000];
+            char[] charData = new char[130000];
+            FileStream file = new FileStream("area.json", FileMode.Open);
+            file.Seek(0, SeekOrigin.Begin);
+            file.Read(byData, 0, 130000); //byData传进来的字节数组,用以接受FileStream对象中的数据,第2个参数是字节数组中开始写入数据的位置,它通常是0,表示从数组的开端文件中向数组写数据,最后一个参数规定从文件读多少字符.
+            Decoder d = Encoding.Default.GetDecoder();
+            d.GetChars(byData, 0, byData.Length, charData, 0);
+            //Console.WriteLine(charData);
+            var str = new string(charData); ;
+            file.Close();
+            var e = JsonConvert.DeserializeObject<Dictionary<string, object>>(str);
+            var list = new List<DistrictInfo>();
+
+            int j = 0;
+            foreach (var i in e)
+            {
+
+                var a = (JObject)i.Value;
+                list.Add(new DistrictInfo { Id = Convert.ToInt32(i.Key), DistrictName = a["name"].ToString(), Pid = 0 });
+                var aqq = a["child"].ToString();
+                if (!string.IsNullOrEmpty(a["child"].ToString()) && j < 31)
+                {
+                    var b = a["child"].ToString();
+                    var aaa = JsonConvert.DeserializeObject<Dictionary<string, object>>(b);
+                    foreach (var x in aaa)
+                    {
+                        var a1 = (JObject)x.Value;
+                        list.Add(new DistrictInfo { Id = Convert.ToInt32(x.Key), DistrictName = a1["name"].ToString(), Pid = Convert.ToInt32(i.Key) });
+                        if (!string.IsNullOrEmpty(a1["child"].ToString()))
+                        {
+                            var a3 = a1["child"].ToString();
+                            var a4 = JsonConvert.DeserializeObject<Dictionary<string, string>>(a3);
+                            foreach (var a5 in a4)
+                            {
+                                list.Add(new DistrictInfo { Id = Convert.ToInt32(a5.Key), DistrictName = a5.Value, Pid = Convert.ToInt32(x.Key) });
+                            }
+                        }
+                    }
+                }
+                j++;
+            }
+            foreach(var m in list)
+            {
+                _iDistrictInfoServices.Add(m);
+                _iDistrictInfoServices.SaverChanges();
+            }
+        }
+
         public ActionResult Login()
         {
             ViewData["LoginValidateCode"] = "123qwertyuiopthnuj";
+            //AddDis();
             return View();
         }
 
